3c2a8410c3b23248bdee3148600e01dceca051ab
Karlhe2002 <xuh011@ucsd.edu>
2022-10-08 09:51:42 -0700

    Update SearchEngine.java

--- SearchEngine.java
+++ SearchEngine.java
@@ -2,39 +2,42 @@ import java.io.IOException;
 import java.net.URI;
 import java.util.*;
 
-class addstring implements URLHandler {
+class Search implements URLHandler {
     ArrayList<String> arrlst = new ArrayList<String>();
 
-    public String addstr(URI url) {
+    public String handleRequest(URI url) {
 
         if (url.getPath().contains("/add")) {
             String[] parameters = url.getQuery().split("=");
             if (parameters[0].equals("add")) {
                 arrlst.add(parameters[1]);
-                for (int i = 0; i < arrlst.size(); i++) {
-                    System.out.println(arrlst[i]);
-                }
-
             }
+            return String.format("A new string (%s) is Added! There are %d strings in the word list", parameters[1],
+                    arrlst.size());
         } else if (url.getPath().contains("/search")) {
-
+            String[] parameters = url.getQuery().split("=");
+            if (parameters[0].equals("s")) {
+                for (String ele : arrlst) {
+                    ArrayList<String> results = new ArrayList<>();
+                    if (ele.contains(parameters[1])) {
+                        results.add(ele);
+                    }
+                }
+            }
         }
-
-        {
-            return "404 Not Found!";
-        }
+        return "404 Not Found!";
     }
 }
 
-class StringServer {
+class SearchEngine {
     public static void main(String[] args) throws IOException {
-        if(args.length == 0){
+        if (args.length == 0) {
             System.out.println("Missing port number! Try any number between 1024 to 49151");
             return;
         }
 
         int port = Integer.parseInt(args[0]);
 
-        Server.start(port, new addstring());
+        Server.start(port, new Search());
     }
 }
